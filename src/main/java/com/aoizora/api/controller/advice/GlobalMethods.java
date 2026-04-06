38c267e3ca0f4a33593c9e0652a6562bfe8bbb6f
Arthur Smirnov <asmirnov@smartru.com>
2026-04-06 20:50:12 +0200

    Добавил обработчики исключения в апи

---
+++ src/main/java/com/aoizora/api/controller/advice/GlobalMethods.java
@@ -0,0 +1,25 @@
+package com.aoizora.api.controller.advice;
+
+import com.aoizora.service.exception.PetNotFoundException;
+import com.aoizora.service.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalMethods {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "User not found")
+    public void userNotFound() throws NoHandlerFoundException {
+
+    }
+
+    @ExceptionHandler(PetNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Pet not found")
+    public void petNotFound() throws NoHandlerFoundException {
+
+    }
+}
